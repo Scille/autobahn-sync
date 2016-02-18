95944909854ab5f8c893f363fe383a5d1e1e408e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-02-18 15:49:55 +0100

    Bump version

--- autobahn_sync/__init__.py
+++ autobahn_sync/__init__.py
@@ -3,7 +3,7 @@ from autobahn_sync.core import (
     AutobahnSync, default_app, publish, call, register, subscribe, start)
 from twisted.internet.error import ConnectionRefusedError  # noqa republishing
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = (
     DEFAULT_AUTOBAHN_ROUTER, DEFAULT_AUTOBAHN_REALM,
     AutobahnSync, default_app, publish, call, register, subscribe, start,
