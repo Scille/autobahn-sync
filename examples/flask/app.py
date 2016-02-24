df449a428831d362b58dca3c2201c0ca5c697fab
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2016-02-24 11:36:00 +0100

    Improve flask example

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,24 +1,31 @@
-from uuid import uuid4
+import os
 from flask import Flask
 from datetime import datetime
 from autobahn_sync.extensions.flask import FlaskAutobahnSync
-from autobahn.wamp import PublishOptions
+from autobahn.wamp import PublishOptions, RegisterOptions
 
 
-worker_id = uuid4().hex
+worker_id = os.getpid()
 app = Flask(__name__)
-serve_history = []
 
 
 def bootstrap():
     wamp = FlaskAutobahnSync(app)
     app.wamp = wamp
+    serve_history = []
 
-    @wamp.subscribe('com.flask_app.page_served')
+    @wamp.subscribe(u'com.flask_app.page_served')
     def page_served(wid, timestamp):
         data = (wid, timestamp)
         print('[Worker %s] Received %s' % (worker_id, data))
         serve_history.append(data)
+
+    # Authorize multiple registers given we can start this app in workers
+    register_opt = RegisterOptions(invoke=u'random')
+    @wamp.register(u'com.flask_app.get_request_history', options=register_opt)
+    def get_request_history(wid):
+        print('[Worker %s] Send request history to worker %s' % (worker_id, wid))
+        return serve_history
 
     return app
 
@@ -30,6 +37,8 @@ def main():
     publish_opt = PublishOptions(exclude_me=False)
     app.wamp.session.publish('com.flask_app.page_served', worker_id,
                              timestamp, options=publish_opt)
+    serve_history = app.wamp.session.call(
+        'com.flask_app.get_request_history', worker_id)
     txts = [
         "<table>",
         "<thead><th>Worker id</th><th>Request timestamp</th></thead>",
@@ -42,4 +51,5 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(port=8081)
+    bootstrap()
+    app.run(port=8081, debug=True)
